2f723f18402581554e2257d36005dddccb436c73
udoh ukamaka ebele <154434643+ukamaka203@users.noreply.github.com>
2024-02-24 03:44:03 -0800

    Add files via upload

---
+++ time.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract time{
+    uint public second = block.timestamp + 1 seconds;
+
+   uint public thirtyseconds = block.timestamp + 30 seconds;
+
+uint public minute = block.timestamp + 3 minutes;
+
+uint public Hour = block.timestamp + 1 hours;
+
+uint public Day = block.timestamp + 1 days;
+
+uint public month = block.timestamp + 32 days;
+
+uint public mi = block.timestamp + 5 minutes;
+}
+
